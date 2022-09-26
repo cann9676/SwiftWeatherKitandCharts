3735001bf36cd861f816c1eb2abc51469e16ec9e
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-09-26 11:36:06 -0400

    Initial Commit

---
+++ SwiftWeatherKitandCharts/SwiftWeatherKitandChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherKitandChartsApp.swift
+//  SwiftWeatherKitandCharts
+//
+//  Created by Chelsea Hannah on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherKitandChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
